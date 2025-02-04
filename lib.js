7b36c57165823934d56044984863dad55667654a
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-02-04 12:38:57 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -316,7 +316,364 @@ var Question = function (questionObj) {
           isCorrect: true
         }
       ]
-    }
+    },
+      {
+        text: "Quel est le langage de programmation principalement utilisé pour le développement d'applications Android ?",
+        answers: [
+          {
+            text: "Java",
+            isCorrect: true
+          },
+          {
+            text: "Python",
+            isCorrect: false
+          },
+          {
+            text: "C++",
+            isCorrect: false
+          },
+          {
+            text: "Swift",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est l'acronyme de 'HTML' ?",
+        answers: [
+          {
+            text: "HyperText Markup Language",
+            isCorrect: true
+          },
+          {
+            text: "HighText Markup Language",
+            isCorrect: false
+          },
+          {
+            text: "Hyper Transfer Markup Language",
+            isCorrect: false
+          },
+          {
+            text: "HyperText Modern Language",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le système d'exploitation développé par Microsoft ?",
+        answers: [
+          {
+            text: "Windows",
+            isCorrect: true
+          },
+          {
+            text: "Linux",
+            isCorrect: false
+          },
+          {
+            text: "MacOS",
+            isCorrect: false
+          },
+          {
+            text: "Android",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le principal composant d'un ordinateur qui exécute des instructions de programmes ?",
+        answers: [
+          {
+            text: "Processeur",
+            isCorrect: true
+          },
+          {
+            text: "Mémoire vive",
+            isCorrect: false
+          },
+          {
+            text: "Disque dur",
+            isCorrect: false
+          },
+          {
+            text: "Carte graphique",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le rôle d'un navigateur web ?",
+        answers: [
+          {
+            text: "Permet de naviguer sur Internet",
+            isCorrect: true
+          },
+          {
+            text: "Protéger les données de l'ordinateur",
+            isCorrect: false
+          },
+          {
+            text: "Stocker les données",
+            isCorrect: false
+          },
+          {
+            text: "Gérer les fichiers locaux",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Que signifie l'acronyme 'CPU' ?",
+        answers: [
+          {
+            text: "Central Processing Unit",
+            isCorrect: true
+          },
+          {
+            text: "Central Power Unit",
+            isCorrect: false
+          },
+          {
+            text: "Computer Power Unit",
+            isCorrect: false
+          },
+          {
+            text: "Control Processing Unit",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quelle est la principale différence entre un SSD et un HDD ?",
+        answers: [
+          {
+            text: "Le SSD est plus rapide et ne contient pas de pièces mobiles",
+            isCorrect: true
+          },
+          {
+            text: "Le SSD est plus lent que le HDD",
+            isCorrect: false
+          },
+          {
+            text: "Le SSD a une capacité plus grande que le HDD",
+            isCorrect: false
+          },
+          {
+            text: "Le HDD est sans fil, alors que le SSD nécessite un câble",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est l'acronyme de 'Wi-Fi' ?",
+        answers: [
+          {
+            text: "Wireless Fidelity",
+            isCorrect: true
+          },
+          {
+            text: "Wide Fidelity",
+            isCorrect: false
+          },
+          {
+            text: "Wireless Frequency",
+            isCorrect: false
+          },
+          {
+            text: "Wide Frequency",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le nom du célèbre moteur de recherche créé par Google ?",
+        answers: [
+          {
+            text: "Google Search",
+            isCorrect: true
+          },
+          {
+            text: "Google Explorer",
+            isCorrect: false
+          },
+          {
+            text: "Google Find",
+            isCorrect: false
+          },
+          {
+            text: "Google Discover",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel langage de programmation est principalement utilisé pour le développement web côté client ?",
+        answers: [
+          {
+            text: "JavaScript",
+            isCorrect: true
+          },
+          {
+            text: "C#",
+            isCorrect: false
+          },
+          {
+            text: "Python",
+            isCorrect: false
+          },
+          {
+            text: "Ruby",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est l'acronyme de 'USB' ?",
+        answers: [
+          {
+            text: "Universal Serial Bus",
+            isCorrect: true
+          },
+          {
+            text: "Uniform Serial Bus",
+            isCorrect: false
+          },
+          {
+            text: "Universal Service Bus",
+            isCorrect: false
+          },
+          {
+            text: "Universal Speed Bus",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Qu'est-ce qu'un 'bug' en informatique ?",
+        answers: [
+          {
+            text: "Un défaut ou une erreur dans un programme",
+            isCorrect: true
+          },
+          {
+            text: "Un type de logiciel malveillant",
+            isCorrect: false
+          },
+          {
+            text: "Un composant matériel",
+            isCorrect: false
+          },
+          {
+            text: "Un type de fichier",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Que signifie l'acronyme 'RAM' ?",
+        answers: [
+          {
+            text: "Random Access Memory",
+            isCorrect: true
+          },
+          {
+            text: "Read Access Memory",
+            isCorrect: false
+          },
+          {
+            text: "Rapid Access Memory",
+            isCorrect: false
+          },
+          {
+            text: "Random Area Memory",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le nom du système d'exploitation développé par Apple pour les ordinateurs ?",
+        answers: [
+          {
+            text: "macOS",
+            isCorrect: true
+          },
+          {
+            text: "Windows",
+            isCorrect: false
+          },
+          {
+            text: "Linux",
+            isCorrect: false
+          },
+          {
+            text: "Android",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est le rôle d'une carte graphique dans un ordinateur ?",
+        answers: [
+          {
+            text: "Traiter et afficher les images à l'écran",
+            isCorrect: true
+          },
+          {
+            text: "Gérer la mémoire vive",
+            isCorrect: false
+          },
+          {
+            text: "Gérer les connexions réseau",
+            isCorrect: false
+          },
+          {
+            text: "Gérer le stockage des données",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel format de fichier est utilisé pour les images vectorielles ?",
+        answers: [
+          {
+            text: "SVG",
+            isCorrect: true
+          },
+          {
+            text: "JPEG",
+            isCorrect: false
+          },
+          {
+            text: "PNG",
+            isCorrect: false
+          },
+          {
+            text: "BMP",
+            isCorrect: false
+          }
+        ]
+      },
+      {
+        text: "Quel est l'objectif principal d'un antivirus ?",
+        answers: [
+          {
+            text: "Protéger l'ordinateur contre les logiciels malveillants",
+            isCorrect: true
+          },
+          {
+            text: "Améliorer la vitesse de l'ordinateur",
+            isCorrect: false
+          },
+          {
+            text: "Optimiser les performances du disque dur",
+            isCorrect: false
+          },
+          {
+            text: "Fournir une interface graphique",
+            isCorrect: false
+          }
+        ]
+      }      
   ];
   
   // variables initialization
